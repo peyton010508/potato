406c3e295edc89ecd1b1d668e04deec88ff5b77f
peyton delong <104819@swcsd.us>
2025-05-09 08:15:04 -0400

    Initial Commit

---
+++ potato/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  potato
+//
+//  Created by peyton delong on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ potato/potatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  potatoApp.swift
+//  potato
+//
+//  Created by peyton delong on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct potatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
